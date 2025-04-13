47728c81f4ad92e3c55e52fa712bc22e62b965be
Hardik <hardikp1351@gmail.com>
2025-04-13 13:52:17 +0530

    db

--- backend/db.js
+++ backend/db.js
@@ -5,9 +5,13 @@ mongoose.connect(process.env.MONGODB_URI)
   .then(() => console.log('Connected to MongoDB'))
   .catch(err => console.error('MongoDB connection error:', err));
 
-const todoSchema = mongoose.schema({
+const todoSchema = new mongoose.schema({
     title: String,
-    descriptiong: String,
-    compeleted: Boolean
+    description: String,
+    completed: Boolean
 })
 
+const todo = mongoose.model('todos', todoSchema)
+module.exports({
+    todo: todo
+})
